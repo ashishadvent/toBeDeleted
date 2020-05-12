1f51138dfec5aab2a4cbd6df08c706263497552d
ashish <ashish.advent88@gmail.com>
2020-05-12 16:14:59 +0530

    first commit

---
+++ justForTestConfig/src/com/TestConfig/checkConfig.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.TestConfig;
+
+/**
+ * @author ashkesar
+ *
+ */
+public class checkConfig {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.print("yes its working fine");
+
+	}
+
+}
